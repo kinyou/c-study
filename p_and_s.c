f75d99f47743731e877bb694cacd536bdadfa9c2
sdeepwang <sdeepwang@localhost.localdomain>
2014-12-22 02:24:32 +0800

    指针和字符串

---
+++ p_and_s.c
@@ -0,0 +1,16 @@
+/*只是把字符串的指针复制了一份给了对应的指针变量*/
+#include <stdio.h>
+
+int main(void){
+
+	char *mesg = "Don't be a fool! ";
+	char *copy;
+
+	copy = mesg;
+
+	printf("%s\n",copy);
+	printf("mesg = %s; &mesg = %p; value = %p\n",mesg,&mesg,mesg);
+	printf("copy = %s; &copy = %p; value = %p\n",copy,&copy,copy);
+
+	return 0;
+}
